3ff37ed6bdd776e022db92aed558a0beb1411eef
lsoni-incise <lokeshsoni1996@gmail.com>
2017-03-08 12:17:02 +0530

    Error Fixed of RTOS Wrapped lib

--- weather-stn/libs/freertos/rtos.c
+++ weather-stn/libs/freertos/rtos.c
@@ -10,35 +10,30 @@
 #include "FreeRTOSConfig.h"
 #include "task.h"
 #include "queue.h"
+#include "semphr.h"
 
 
-uint8_t rtos_TaskCreate(TaskHandle_t pvTaskCode, const char * const pcName, uint16_t usStackDepth, void *pvParameters, UBaseType_t uxPriority, TaskHandle_t *pxCreatedTask);
+uint8_t rtos_TaskCreate(TaskFunction_t pvTaskCode, const char * const pcName, uint16_t usStackDepth, void *pvParameters, UBaseType_t uxPriority, TaskHandle_t *pxCreatedTask)
 {
 	uint8_t Status_Task;
 	Status_Task =xTaskCreate( pvTaskCode, pcName, usStackDepth, pvParameters, uxPriority, pxCreatedTask);
     return Status_Task;
 }
-void rtos_Scheduler()
+void rtos_Delay(x)
 {
-	vTaskStartScheduler();
-	return PASS;
+	UBaseType_t time;
+	//time = pdMS_TO_TICKS(x);
+	vTaskDelay(x);
 }
-uint8_t rtos_CreateQueue(QueueHandle_t Create_Queue, uint16_t Length, uint16_t Size )
+
+QueueHandle_t rtos_CreateQueue(QueueHandle_t Create_Queue, uint16_t Length, uint16_t Size )
 {
 	Create_Queue = xQueueCreate( Length , Size );
 	return Create_Queue;
 }
-void rtos_Delay(uint16_t x)
-{
-	vTaskDelay(x);
-}
 
-void rtos_DeleteTask(TaskHandle_t pvParameter)
-{
-	vTaskDelete(pvParameter);
-}
 
-uint8_t rtos_QueueSendIntData(QueueHandle_t xQueue, uint8_t *pvItemToQueue,TickType_t xTicksToWait)
+UBaseType_t rtos_QueueSendIntData(QueueHandle_t xQueue, uint8_t *pvItemToQueue,TickType_t xTicksToWait)
 {
 		UBaseType_t Queue_Status;
 		Queue_Status = xQueueSend(xQueue, pvItemToQueue, xTicksToWait);
@@ -57,7 +52,7 @@ uint8_t rtos_QueueRecieveIntData(QueueHandle_t xQueue, uint8_t *pvBuffer,TickTyp
 {
 	UBaseType_t Queue_Status;
 	Queue_Status =  xQueueReceive(xQueue, pvBuffer, xTicksToWait);
-	return Queue_Status
+	return Queue_Status;
 }
 
 uint8_t rtos_QueueRecieveCharData(QueueHandle_t xQueue, UBaseType_t *pvBuffer, TickType_t xTicksToWait)
@@ -67,22 +62,22 @@ uint8_t rtos_QueueRecieveCharData(QueueHandle_t xQueue, UBaseType_t *pvBuffer, T
 	return Queue_Status;
 }
 
-uint8_t rtos_SemaphoreBinary(SemaphoreHandle_t Semaphore)
-{
-	UBaseType_t Status_Binary = vSemaphoreCreateBinary(Semaphore);
-	return Status_Binary;
-}
-
-BaseType_t rtos_SemaphorTake(SemaphoreHandle_t Handle , TickType_t Wait_Time)
-{
-	BaseType_t Status_Take;
-	Status_Take = xSemaphoreTake(Handle);
-	return Status;
-}
-
 BaseType_t rtos_SemaphorGive(SemaphoreHandle_t Handle)
 {
-	uint8_t Status_Give;
-	Status_Give = xSemaphoreGive(Handle);
-	return Status_Give;
+	BaseType_t T;
+	T = xSemaphoreGive(Handle);
+	return T;
 }
+BaseType_t rtos_SemaphorTake(SemaphoreHandle_t Handle ,TickType_t Wait_Time)
+{ 
+	BaseType_t T;
+	T = xSemaphoreTake(Handle,Wait_Time);
+	return T;
+}
+
+/*SemaphoreHandle_t  rtos_SemaphoreBinary(SemaphoreHandle_t Semaphore)
+{
+	SemaphoreHandle_t Test;
+	Test = vSemaphoreCreateBinary( Semaphore );
+	return Test;	
+}*/
--- weather-stn/libs/freertos/rtos.h
+++ weather-stn/libs/freertos/rtos.h
@@ -6,23 +6,31 @@
  */ 
 
 
-#ifndef RTOS_H_
-#define RTOS_H_
+#ifndef RTOS_H
+#define RTOS_H
+
+#include <stdint.h>
+#include "FreeRTOS.h"
+#include "queue.h"
+#include "semphr.h"
 #define PASS 1
 #define FAIL 0
 
+#define  rtos_Scheduler() vTaskStartScheduler()
+#define  rtos_DeleteTask(pvParameter) vTaskDelete(pvParameter)
+//#define  rtos_SemaphoreBinary(Semaphore) vSemaphoreCreateBinary( Semaphore )
 
-uint8_t rtos_TaskCreate(TaskHandle_t pvTaskCode, const char * const pcName, uint16_t usStackDepth, void *pvParameters, UBaseType_t uxPriority, TaskHandle_t *pxCreatedTask);
-void rtos_Scheduler();
-uint8_t rtos_CreateQueue(QueueHandle_t Create_Queue, uint16_t Length, uint16_t Size );
-void rtos_Delay(uint16_t x);
-void rtos_DeleteTask(TaskHandle_t pvParameter);
-uint8_t rtos_QueueSendIntData(QueueHandle_t xQueue, uint8_t *pvItemToQueue,TickType_t xTicksToWait);
+//#define rtos_SemaphorTake(Handle ,Wait_Time) xSemaphoreTake(Handle,Wait_Time)
+//#define  rtos_SemaphorGive(Handle) xSemaphoreGive(Handle)
+
+uint8_t rtos_TaskCreate(TaskFunction_t pvTaskCode, const char * const pcName, uint16_t usStackDepth, void *pvParameters, UBaseType_t uxPriority, TaskHandle_t *pxCreatedTask);
+QueueHandle_t rtos_CreateQueue(QueueHandle_t Create_Queue, uint16_t Length, uint16_t Size );
+void rtos_Delay(x);
+UBaseType_t rtos_QueueSendIntData(QueueHandle_t xQueue, uint8_t *pvItemToQueue,TickType_t xTicksToWait);
 uint8_t rtos_QueueSendCharData(QueueHandle_t xQueue, UBaseType_t *pvItemToQueue,TickType_t xTicksToWait);
 uint8_t rtos_QueueRecieveIntData(QueueHandle_t xQueue, uint8_t *pvBuffer,TickType_t xTicksToWait);
 uint8_t rtos_QueueRecieveCharData(QueueHandle_t xQueue, UBaseType_t *pvBuffer,TickType_t xTicksToWait);
-uint8_t rtos_SemaphoreBinary(SemaphoreHandle_t Semaphore);
 BaseType_t rtos_SemaphorGive(SemaphoreHandle_t Handle);
 BaseType_t rtos_SemaphorTake(SemaphoreHandle_t Handle , TickType_t Wait_Time);
-
+void  rtos_SemaphoreBinary(SemaphoreHandle_t Semaphore);
 #endif /* RTOS_H_ */
