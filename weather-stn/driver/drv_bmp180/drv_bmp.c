b1b3a87b8eba4bbddb41d97e2c73233c5c371d8b
lsoni-incise <lokeshsoni1996@gmail.com>
2017-03-10 15:28:02 +0530

    Documantation of BMP180 is completed.

--- weather-stn/driver/drv_bmp180/drv_bmp.c
+++ weather-stn/driver/drv_bmp180/drv_bmp.c
@@ -17,6 +17,44 @@
 /*	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.                             */
 /************************************************************************************************/
 
+/****************************************************************************************************************************************/
+/*  BMP180  Pressure and Temperature Sensor 																							*/
+/*	Works on I2C Protocol																												*/
+/*	BMP180 Connection																													*/
+/*	SCL - A5;																															*/
+/*	SDA - A4;																															*/
+/*	These pins should be connected with pullup register ( 2 to 10 KOhm)																	*/
+/*	Supply voltage: 1.8 ... 3.6V (VDD)																									*/
+/*	Temperature range:	-40 to +85																										*/
+/*	Pressure range:	300 ... 1100 hPa																									*/
+/*	Algorithm :																															*/
+/*		1. Start -> Module ADD -> Register Address -> Restart ->Measurnment Control ->Restart											*/
+/* 		Module Address: 0xEE																											*/
+/* 		Register Address:																												*/
+/* 																																		*/
+/* 		Register Name  | Register Adress	|	bit7	|	bit6  | 	bit5  |	bit4  |	bit3  |	bit2  |	bit1  |	bit0  |	Reset state		*/
+/* 		out_xlsb	   |	F8h				|					    	adc_out_xlsb<7:3>	  |	 0	  |	  0	  |	  0	  |		00h			*/
+/* 	 	out_lsb		   |	F7h				|						 	adc_out_lsb<7:0>							  |		00h			*/
+/* 		out_msb		   |	F6h				|			|		  |    adc_out_msb<7:0>								  |		80h			*/
+/* 		ctrl_meas	   | 	F4h				|		oss<1:0> 	  |  	sco	  |			measurement control			  |		00h			*/
+/* 		soft reset	   |	E0h				|			|		  |				reset								  |		00h			*/
+/* 		id			   |	D0h				|							id<7:0>										  |		55h			*/
+/* 	 	calib21	calib0 | BFh downto AAh		|			calib21<7:0> downto calib0<7:0>								  |		n/a			*/
+/* 	 																																	*/
+/*	 	Measurement 	  Register	Max Time (ms)																						*/
+/* 	 	Temperature 		0x2E		4.5																								*/
+/*	 	Pressure (oss = 0) 	0x34		4.5																								*/
+/*	 	Pressure (oss = 1) 	0x74		7.5																								*/
+/*	 	Pressure (oss = 2) 	0xB4		13.5																							*/
+/* 	 	Pressure (oss = 3) 	0xF4		25.5																							*/
+/*																																		*/
+/*		2. After restart ( See step 1 ) -> Module Address(0xEE write) -> Register Address (i.e F6-MSB, F7-LSB ) -> Restart				*/
+/*		3. After restart ( See step 2) -> Module Add (0xEF read) -> Read ( MSB) -> Read (LSB)											*/
+/*																																		*/
+/*																																		*/
+/****************************************************************************************************************************************/
+
+
 
 int init_bmp(void)
 {
